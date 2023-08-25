1934bfa09a9341380a7e171984cde16fcdcbed14
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2023-08-24 21:19:21 -0400

    Add files via upload

---
+++ attack_detection.c
@@ -0,0 +1,97 @@
+#include "attack_detection.h"
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/if_ether.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SCAN_THRESHOLD 10 // Threshold for SYN packets to detect port scanning
+#define TIME_WINDOW 60   // Time window in seconds to consider for port scanning
+
+// Structure to keep track of source IPs for port scanning detection
+struct SourceEntry {
+  in_addr_t source_ip; // Source IP address
+  int syn_count;       // Count of SYN packets
+  time_t last_time;    // Last time a SYN packet was received from this source
+  struct SourceEntry *next; // Next entry in the list
+};
+
+struct SourceEntry *sources = NULL; // Linked list head for source entries
+
+// Function to log alerts to a file
+void LogAlertToFile(const char *attack_name) {
+  FILE *file = fopen("alerts.log", "a");
+  if (file == NULL) {
+    perror("Could not open alerts.log");
+    return;
+  }
+  fprintf(file, "ALERT: %s\n", attack_name);
+  fclose(file);
+}
+
+// Function to alert an attack
+void AlertAttack(const char *attack_name) {
+  printf("ALERT: %s\n", attack_name);
+  LogAlertToFile(attack_name); // Log the alert to a file
+}
+
+// Function to analyze TCP packets for various attack patterns
+void AnalyzeTcpAttack(const struct pcap_pkthdr *header, const u_char *packet) {
+  struct ip *ip_header = (struct ip *)(packet + sizeof(struct ethhdr));
+  struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ethhdr) + ip_header->ip_hl * 4);
+
+  // Check for SYN-Flood attack pattern
+  if (tcp_header->syn && !tcp_header->ack) {
+    AlertAttack("Possible SYN-Flood attack detected");
+  }
+
+  // Check for XMAS attack pattern
+  if (tcp_header->fin && tcp_header->urg && tcp_header->psh) {
+    AlertAttack("Possible XMAS attack detected");
+  }
+
+  // Check for Port Scanning attack pattern
+  if (tcp_header->syn && !tcp_header->ack) {
+    struct SourceEntry *current = sources;
+    time_t now = time(NULL);
+
+    // Iterate through linked list of sources
+    while (current != NULL) {
+      if (current->source_ip == ip_header->ip_src.s_addr) {
+        if (current->last_time < now - TIME_WINDOW) {
+          current->syn_count = 1; // Reset count if outside the time window
+        } else {
+          current->syn_count++;
+          if (current->syn_count >= SCAN_THRESHOLD) {
+            AlertAttack("Possible Port Scanning attack detected");
+          }
+        }
+        current->last_time = now;
+        return;
+      }
+      current = current->next;
+    }
+
+    // Add a new source entry if not found
+    struct SourceEntry *new_source = malloc(sizeof(struct SourceEntry));
+    new_source->source_ip = ip_header->ip_src.s_addr;
+    new_source->syn_count = 1;
+    new_source->last_time = now;
+    new_source->next = sources;
+    sources = new_source;
+  }
+
+  // Add more TCP attack analysis as needed
+}
+
+// Function to analyze UDP packets for specific attack patterns
+void AnalyzeUdpAttack(const struct pcap_pkthdr *header, const u_char *packet) {
+  // Analyze UDP packets here
+
+  // Example: UDP Flood attack detection
+  // More logic needed to determine UDP Flood
+
+  // Add more UDP attack analysis as needed
+}
---
+++ packet_analysis.c
@@ -0,0 +1,47 @@
+#include "packet_analysis.h"
+#include "attack_detection.h"
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/if_ether.h>
+#include <stdio.h>
+
+// Log packet details
+void LogPacket(const char *protocol, const char *info) {
+  printf("%s Packet: %s\n", protocol, info);
+}
+
+// Analyze TCP packets for specific attack patterns
+void AnalyzeTcpPacket(const struct pcap_pkthdr *header, const u_char *packet) {
+  struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ethhdr) + sizeof(struct ip));
+  char info[100];
+  snprintf(info, sizeof(info), "Source Port: %d, Dest Port: %d", ntohs(tcp_header->source), ntohs(tcp_header->dest));
+  LogPacket("TCP", info);
+  AnalyzeTcpAttack(header, packet); // Function for attack analysis
+}
+
+// Analyze UDP packets for specific attack patterns
+void AnalyzeUdpPacket(const struct pcap_pkthdr *header, const u_char *packet) {
+  struct udphdr *udp_header = (struct udphdr *)(packet + sizeof(struct ethhdr) + sizeof(struct ip));
+  char info[100];
+  snprintf(info, sizeof(info), "Source Port: %d, Dest Port: %d", ntohs(udp_header->source), ntohs(udp_header->dest));
+  LogPacket("UDP", info);
+  AnalyzeUdpAttack(header, packet); // Function for attack analysis
+}
+
+void AnalyzePacket(const struct pcap_pkthdr *header, const u_char *packet) {
+  struct ip *ip_header = (struct ip *)(packet + sizeof(struct ethhdr));
+
+  // Determine the protocol and analyze accordingly
+  switch (ip_header->ip_p) {
+    case IPPROTO_TCP:
+      AnalyzeTcpPacket(header, packet);
+      break;
+    case IPPROTO_UDP:
+      AnalyzeUdpPacket(header, packet);
+      break;
+    default:
+      // Unsupported protocol
+      break;
+  }
+}
